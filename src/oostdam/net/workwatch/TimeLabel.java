826f5993c82164344c30e02d446cf24369884014
Thijs <oostdam@gmail.com>
2018-12-15 02:28:24 +0100

    refactor some

--- src/oostdam/net/workwatch/TimeLabel.java
+++ src/oostdam/net/workwatch/TimeLabel.java
@@ -1,16 +1,51 @@
 package oostdam.net.workwatch;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.control.Label;
 
+import java.time.Duration;
 import java.time.Instant;
 
-public class TimeLabel {
+/**
+ * Custom javafx node
+ * It keeps track of time and exposes the ability to modify the time.
+ */
+public class TimeLabel extends Label{
 
-    private Label label;
     private final Instant start = Instant.now();
-    private int additions;
+    private Duration modification = Duration.ZERO;
 
     public TimeLabel(){
+        Timeline clock = new Timeline(
+                new KeyFrame(javafx.util.Duration.ZERO, e -> { updateTime(); })
+                , new KeyFrame(javafx.util.Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
 
+    public void modify(Duration duration){
+        modification = modification.plus(duration);
+        updateTime();
+    }
+
+    private void updateTime(){
+        Duration duration = Duration.between(start, Instant.now()).plus(modification);
+        this.setText("Time: " + format(duration));
+    }
+
+    /**
+     * Format a duration to hh:MM:SS
+     */
+    public String format(Duration duration){
+        long seconds = duration.getSeconds();
+        long absSeconds = Math.abs(seconds);
+        String positive = String.format(
+                "%02d:%02d:%02d",
+                absSeconds / 3600,
+                (absSeconds % 3600) / 60,
+                absSeconds % 60);
+        return seconds < 0 ? "-" + positive : positive;
     }
 }
--- src/oostdam/net/workwatch/WorkTimerController.java
+++ src/oostdam/net/workwatch/WorkTimerController.java
@@ -1,24 +1,16 @@
 package oostdam.net.workwatch;
 
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 public class WorkTimerController {
 
-    private final Instant start = Instant.now();
-    private int additions;
-
     @FXML
-    private Label time;
+    private TimeLabel time;
     @FXML
     private Button minus15;
     @FXML
@@ -26,25 +18,11 @@ public class WorkTimerController {
 
     @FXML
     private void initialize(){
-        updateTime();
-        plus15.setText(" +15");
-        plus15.setOnAction(event -> this.add15Minutes());
-        minus15.setText(" -15");
-        minus15.setOnAction(event -> this.subtract15Minutes());
+        plus15.setText(" +15 min");
+        plus15.setOnAction(event -> time.modify(Duration.of(15, ChronoUnit.MINUTES)));
+        minus15.setText(" -15 min");
+        minus15.setOnAction(event -> time.modify(Duration.of(-15, ChronoUnit.MINUTES)));
 
-        Timeline clock = new Timeline(
-                new KeyFrame(javafx.util.Duration.ZERO, e -> { updateTime(); })
-                , new KeyFrame(javafx.util.Duration.seconds(1)));
-        clock.setCycleCount(Animation.INDEFINITE);
-        clock.play();
+
     }
-
-    private void subtract15Minutes(){additions--; updateTime();}
-    private void add15Minutes(){additions++; updateTime();}
-
-    private void updateTime(){
-        Duration duration = Duration.between(start, Instant.now()).plus(additions * 15, ChronoUnit.MINUTES);
-        time.setText("Time: " + DurationFormatter.format(duration));
-    }
-
 }
